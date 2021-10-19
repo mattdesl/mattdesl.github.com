33725deb974cc619e9317a50ca623c0a4c99b33b
mattdesl <dave.des@gmail.com>
2021-10-19 12:45:14 +0100

    fix things

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // assetsInclude: ["./embed/meridian.html", "src/embed/meridian.min.js"],
+  build: {
+    outDir: "app",
+  },
 });
