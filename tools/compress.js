13dffed070ff024b758a89f5631f39733baeb31c
matt <matt@matts-MacBook-Air.local>
2023-03-23 12:21:27 +0000

    new website

--- tools/compress.js
+++
@@ -1,6 +0,0 @@
-var through = require('through2')
-module.exports = function () {
-  return through.obj(function (repo, enc, next) {
-    this.push({ url: repo.demo, repository: repo.html_url, name: repo.full_name })
-  })
-}
